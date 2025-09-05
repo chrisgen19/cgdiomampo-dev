b864a443e2c36f01a977fcf553bef11cdc73f5b4
Christian Genesis Diomampo <253071.CDiomampo@gbs.local>
2025-09-05 15:55:57 +0800

    add ga4

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,29 @@
+import Script from 'next/script';
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* --- Google Analytics Scripts Start --- */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+          `,
+        }}
+      />
+      {/* --- Google Analytics Scripts End --- */}
+
+      <Component {...pageProps} />
+    </>
+  );
 }
