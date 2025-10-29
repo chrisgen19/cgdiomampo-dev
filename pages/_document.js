2552c0ddc4c5ceff9d52284fce942185ff6725a3
Christian Diomampo <chrisgen19@gmail.com>
2025-10-29 08:14:53 +0800

    add google search console tag

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Google Search Console Verification */}
+        {/* Replace YOUR_VERIFICATION_CODE with the actual code from Google Search Console */}
+        <meta name="google-site-verification" content="uRp-UbI0NWG6cjziZS9a40Y0BNqg2dFX-oVehIhyOUY" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
